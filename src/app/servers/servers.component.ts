e8b5d0c9246e9680c711d9d0c07497d58d64b714
Vinícius Romão <viniciusromao.contato@gmail.com>
2020-07-20 23:54:31 -0300

    learning: data binding react to user events (click)

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -9,16 +9,19 @@ import { Component, OnInit } from '@angular/core';
   //   <app-server></app-server>
   // `,
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  styleUrls: ['./servers.component.css'],
 })
 export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
+  serverCreationStatus: string = 'No server was created!';
 
   constructor() {
-    setTimeout(() => this.allowNewServer = true, 2000);
+    setTimeout(() => (this.allowNewServer = true), 2000);
   }
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
+  onCreateServer() {
+    this.serverCreationStatus = 'Server was created!';
+  }
 }
