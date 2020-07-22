18df5716ac199ae745ace629ce75a9752703e1bd
Vinícius Romão <viniciusromao.contato@gmail.com>
2020-07-22 02:41:11 -0300

    learning: ngIf structural

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -15,6 +15,7 @@ export class ServersComponent implements OnInit {
   allowNewServer: boolean = false;
   serverCreationStatus: string = 'No server was created!';
   serverName: string = '';
+  serverCreated: boolean = false;
 
   constructor() {
     setTimeout(() => (this.allowNewServer = true), 2000);
@@ -23,6 +24,7 @@ export class ServersComponent implements OnInit {
   ngOnInit(): void {}
 
   onCreateServer() {
+    this.serverCreated = true;
     this.serverCreationStatus = `Server was created! Name is ${this.serverName}`;
   }
 
